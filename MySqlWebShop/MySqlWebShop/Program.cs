c4dd2e1763d9d3bf77c20abbfa44e9b43218d22b
Mátrai Gergő <matraigergo.1124@gmail.com>
2023-09-17 10:09:49 +0200

    Initial commit

---
+++ MySqlWebShop/MySqlWebShop/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace MySqlWebShop
+{
+    internal class Program
+    {
+        static MySqlConnection connection = new MySqlConnection("host=localhost;user=root;pwd=;database=webshop;");
+
+        static void Main(string[] args)
+        {
+            connection.Open();
+
+            //4. feladat
+            string query = "SELECT DISTINCT megnevezes, bruttoegysegar FROM termekek INNER JOIN szamlatetel ON szamlatetel.termekid = termekek.id;";
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            MySqlDataReader read = cmd.ExecuteReader();
+
+            Console.WriteLine("4.feladat:");
+            while (read.Read())
+            {
+                Console.WriteLine($"{read[0]} {read[1]}");
+            }
+            read.Close();
+            connection.Close();
+
+            //5. feladat
+            string valtozatas = "UPDATE termekek SET ar = ar - ar * 0.05 WHERE ar > 5000";
+            //MySqlCommand cmd2 = new MySqlCommand(valtozatas, connection);
+            //cmd2.ExecuteNonQuery();
+            Console.WriteLine("5. feladat: 5000 Ft-nál drágább termékek 5%-al olcsóbbak lettek.");
+
+            //6. feladat
+            connection.Open();
+            string query2 = "SELECT SUM(bruttoegysegar * mennyiseg) FROM szamlatetel INNER JOIN szamlafej ON szamlafej.id = szamlatetel.szamlafejid WHERE teljesites > '2018-01-01' AND teljesites < '2018-01-15'";
+            MySqlCommand cmd3 = new MySqlCommand(query2, connection);
+            MySqlDataReader read2 = cmd3.ExecuteReader();
+
+            while (read2.Read())
+            {
+                Console.WriteLine($"6. feladat: Webshop bruttó árbevétele: {read2[0]} Ft");
+            }
+            read2.Close();
+            connection.Close();
+
+            //7. feladat
+            connection.Open();
+            string query3 = "SELECT nev, telepules FROM vevok LEFT JOIN szamlafej ON szamlafej.vevoid = vevok.id WHERE kelt IS NULL ORDER BY nev ASC;";
+            MySqlCommand cmd4 = new MySqlCommand(query3, connection);
+            MySqlDataReader read3 = cmd4.ExecuteReader();
+
+            Console.WriteLine("7. feladat:");
+            while (read3.Read())
+            {
+                Console.WriteLine($"{read3[0]} {read3[1]}");
+            }
+            read3.Close();
+            connection.Close();
+
+            //8. feladat
+            connection.Open();
+            string query4 = "SELECT megnevezes, SUM(bruttoegysegar * mennyiseg) AS 'bevetel' FROM szamlatetel INNER JOIN termekek ON termekek.id = szamlatetel.termekid GROUP BY megnevezes ORDER BY bevetel DESC LIMIT 3;";
+            MySqlCommand cmd5 = new MySqlCommand(query4, connection);
+            MySqlDataReader read4 = cmd5.ExecuteReader();
+
+            Console.WriteLine("8. feladat:");
+            while (read4.Read())
+            {
+                Console.WriteLine($"{read4[0]} {read4[1]}");
+            }
+            read4.Close();
+            connection.Close();
+        }
+    }
+}
---
+++ MySqlWebShop/tablak.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE IF NOT EXISTS webshop DEFAULT charset utf8 COLLATE utf8_hungarian_ci;
+
+Create table termekek (
+	id int not null AUTO_INCREMENT PRIMARY key,
+    megnevezes varchar(40) not null UNIQUE,
+    ar numeric(10,0)
+);
+
+create table vevok (
+	id int not null AUTO_INCREMENT PRIMARY key,
+	nev varchar(100) not null,
+	iranyitoszam varchar(4),
+	telepules varchar(50),
+	utcahazszam varchar(100)
+);
+
+create table szamlafej (
+	id int not null AUTO_INCREMENT PRIMARY key,
+	szamlaszam varchar(30) not null unique,
+	kelt datetime not null,
+	teljesites datetime not null,
+	vevoid int not null
+);
+
+create table szamlatetel (
+	id int not null AUTO_INCREMENT PRIMARY key,
+	szamlafejid int not null,
+	termekid int not null,
+	mennyiseg int not null,
+	mennyisegiegyseg varchar(20) not null,
+	bruttoegysegar numeric(10,0) not null,
+	afaszazalek int not null
+);
